f535ba09be7984cbc850f1f8281508bbcd01de80
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-11-11 08:44:03 +0800

    feat: add keystatic config

---
+++ keystatic.config.ts
@@ -0,0 +1,57 @@
+import {
+  config,
+  fields,
+  collection,
+  type LocalConfig,
+  type GitHubConfig,
+} from "@keystatic/core";
+
+const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
+  process.env.NODE_ENV === "development"
+    ? { kind: "local" }
+    : {
+        kind: "github",
+        repo: {
+          owner: "zakiego",
+          name: "ramadan-countdown",
+        },
+      };
+
+export const keystaticConfig = config({
+  storage,
+  collections: {
+    ramadan: collection({
+      label: "Ramadan",
+      slugField: "year",
+      path: "src/content/ramadan/*",
+      format: { data: "json" },
+      schema: {
+        year: fields.slug({ name: { label: "Ramadan Year" } }),
+        ramadanStart: fields.date({
+          label: "Ramadan Start",
+          validation: {
+            isRequired: true,
+          },
+        }),
+        ramadanEnd: fields.date({
+          label: "Ramadan End",
+          validation: {
+            isRequired: true,
+          },
+        }),
+        eidAlFitr: fields.date({
+          label: "Eid Al-Fitr",
+          validation: {
+            isRequired: true,
+          },
+        }),
+        notes: fields.document({
+          label: "Notes",
+          formatting: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
