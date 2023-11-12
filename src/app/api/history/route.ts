62082b62e1116d65776eb72f20ecdba67645f269
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-11-12 17:20:01 +0800

    feat: add history ramadan API

---
+++ src/app/api/history/route.ts
@@ -0,0 +1,26 @@
+import { getNextRamadan } from "@/app/(home)/utils";
+import { createCountdown } from "@/utils/countdown";
+import { keystaticReader } from "@/utils/keystatic";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function GET(request: NextRequest) {
+  const schema = z.array(
+    z.object({
+      slug: z.coerce.number(),
+      entry: z.object({
+        year: z.coerce.number(),
+        ramadanStart: z.coerce.date(),
+        ramadanEnd: z.coerce.date(),
+        eidAlFitr: z.coerce.date(),
+        notes: z.any(),
+      }),
+    }),
+  );
+
+  const data = await schema.parseAsync(
+    await keystaticReader.collections.ramadan.all(),
+  );
+
+  return NextResponse.json(data.map((ramadan) => ramadan.entry));
+}
