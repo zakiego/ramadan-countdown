04c4fc9bdedcab49b728953ed657ce6d9ed11bcb
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-11-12 17:05:16 +0800

    Refactor metadata and add API link to page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,37 @@
 import Countdown from "@/app/(home)/countdown";
 import { getNextRamadan } from "@/app/(home)/utils";
-import { Metadata } from "next";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-export const metadata: Metadata = {
-  title: "Ramadan Countdown",
-  description: "Ramadan Countdown",
-};
+export async function generateMetadata() {
+  const api = await getNextRamadan();
+
+  return {
+    title: `Ramadan Countdown ${api.entry.year}`,
+    description: `Ramadan Countdown ${api.entry.year}`,
+  };
+}
 
 export default async function Page() {
   const api = await getNextRamadan();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-white to-gray-100 ">
-      <h1 className="text-4xl font-bold text-gray-800  mb-6">
+    <div className="relative flex flex-col items-center pt-20 md:pt-0 md:justify-center min-h-screen bg-gradient-to-r from-white to-gray-100 px-4 md:px-0">
+      <h1 className="text-4xl font-bold text-gray-800 mb-6 text-center">
         Ramadan Countdown
       </h1>
       <Countdown nextRamadan={api.entry.ramadanStart} />
+      <div className="absolute bottom-4 opacity-70">
+        <p className="text-gray-600 text-sm">
+          API available{" "}
+          <a
+            href="/api/countdown?timezoneOffset=8"
+            className="text-blue-500 hover:underline"
+          >
+            here
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
