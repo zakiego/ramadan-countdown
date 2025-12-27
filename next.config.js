348bcd2241dea675bcaa447eabea1d11ff0e774d
M. Zakiyuddin Munziri <zakiyuddinmunziri@gmail.com>
2025-12-28 01:43:09 +0800

    refactor: remove Cloudflare configuration and related scripts

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-initOpenNextCloudflareForDev();
 
 export default nextConfig;
